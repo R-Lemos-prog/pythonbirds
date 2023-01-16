44b5ad7230c22cfcae8a68776e50fd345bf8f426
Raiene Lemos <raienelemos@outlook.com>
2023-01-16 11:40:09 -0300

    Criado atributo complexo 'filhos'.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=16):
+    def __init__(self, *filhos, nome=None, idade=5):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá, {id(self)}!'
+        return f'Olá, objeto de id "{id(self)}"!'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Catarina')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Raiene'
-    print(p.nome)
-    print(p.idade)
-    p.idade = 23
-    print(p.idade)
-    print(f'{p.nome} tem {p.idade} anos de idade.')
+    catarina = Pessoa(nome='Catarina')
+    raiene = Pessoa(catarina, nome='Raiene', idade= 23)
+    print(Pessoa.cumprimentar(catarina))
+    print(f'{catarina.nome} tem {catarina.idade} anos de idade.')
+    print(f'{raiene.nome} tem {raiene.idade} anos de idade.')
+    for filho in raiene.filhos:
+        print(f'{filho.nome} com {filho.idade} anos de idade.')
